3fc4cc7adf9e2ae7aa33db8a315aa028c1a90f6b
MendezCarl <mendez.c@ufl.edu>
2026-05-09 21:37:23 -0400

    Setup the github actions to deploy my website

--- project/src/data/photos.ts
+++ project/src/data/photos.ts
@@ -1,5 +1,7 @@
+const base = import.meta.env.BASE_URL
+
 export const photos = [
-  { src: '/photos/IMG_5916.jpg', caption: 'Attended Hack@Brown 2026 and won 1st in Fetch.ai challenge' },
-  { src: '/photos/IMG_9280.jpg', caption: 'Attended HooHacks 2026 with Anthony Le and met Swift MLH CEO' },
-  { src: '/photos/lahacks.jpeg', caption: 'Attended LAHacks 2026 with Anthony Le and met Shervin and Sina. Took a photo with Himanshu Verma to commerate this moment during project showcase.' },
+  { src: `${base}photos/IMG_5916.jpg`, caption: 'Attended Hack@Brown 2026 and won 1st in Fetch.ai challenge' },
+  { src: `${base}photos/IMG_9280.jpg`, caption: 'Attended HooHacks 2026 with Anthony Le and met Swift MLH CEO' },
+  { src: `${base}photos/lahacks.jpeg`, caption: 'Attended LAHacks 2026 with Anthony Le and met Shervin and Sina. Took a photo with Himanshu Verma to commerate this moment during project showcase.' },
 ]
